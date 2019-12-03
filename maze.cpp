732c196adbaf6b369aba925d8192acd0f71fd181
Qian Chen <elgs1980@hotmail.com>
2019-12-02 19:35:28 -0800

    Added maze.cpp.

---
+++ maze.cpp
@@ -0,0 +1,14 @@
+/**
+ * This is to implement an SAS (State-Action-State) transitioning model
+ * for reinformcement learning.  
+ */
+
+
+#include <iostream>
+
+int main()
+{
+  int** maze;
+  std::cout << "hello" << std::endl;
+  return 0;
+}
