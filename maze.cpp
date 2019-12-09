8dd87d0bd667b9e2d59255a1e6f1d02ae9309436
Qian Chen <elgs1980@hotmail.com>
2019-12-08 18:19:38 -0800

    Added value access methods on State.

--- maze.cpp
+++ maze.cpp
@@ -7,8 +7,10 @@
 
 using namespace std;
 
-const int ROWS = 10;
-const int COLS = 10;
+#define DEBUG
+
+#define ROWS 10
+#define COLS 10
 
 enum MoveDirection { UP, DOWN, LEFT, RIGHT };
 enum ActionType { EXPLORATION, EXPLOITATION };
@@ -17,11 +19,37 @@ struct State {
   int* data;
   State() {
     this->data = new int[ROWS * COLS];
-    cout << sizeof this->data[0] << endl;
-    memset(this->data, 0xFF, sizeof(int) * ROWS * COLS);
+#ifdef DEBUG
+    cout << "Data created." << endl;
+#endif
+    memset(this->data, 0, sizeof(int) * ROWS * COLS);
   }
 
-  ~State() { delete[] this->data; }
+  ~State() {
+    delete[] this->data;
+#ifdef DEBUG
+    cout << "Data cleaned up." << endl;
+#endif
+  }
+
+  void print() {
+    for (int i = 0; i < ROWS; ++i) {
+      for (int j = 0; j < COLS; ++j) {
+        cout << this->data[i * COLS + j];
+      }
+      cout << endl;
+    }
+  }
+
+  void setToAt(int value, int row, int col) {
+    this->data[row * COLS + col] = value;
+  }
+
+  void setByAt(int value, int row, int col) {
+    this->data[row * COLS + col] += value;
+  }
+
+  int getAt(int row, int col) { return this->data[row * COLS + col]; }
 };
 
 struct Action {
@@ -31,5 +59,9 @@ struct Action {
 
 int main() {
   State state;
+  state.setToAt(3, 3, 3);
+  state.setByAt(5, 3, 3);
+  state.print();
+  cout << state.getAt(3, 3) << endl;
   return 0;
 }
